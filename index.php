fe24e7c9eacd4e0f0ddf14a5d47c75f54670f266
IvanRoperto <roperto@gmail.com>
2019-03-11 13:11:04 -0400

    Correccion de los funcionamientos de los botones siguientes y previos del slideshow

--- index.php
+++ index.php
@@ -43,11 +43,11 @@
         <p>Equipos de alto volumen y multifuncionales.</p>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" onclick="$('#carouselExampleSlidesOnly').carousel('prev')">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" onclick="$('#carouselExampleSlidesOnly').carousel('prev')">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
