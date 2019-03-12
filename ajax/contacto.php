0839012347d2296cb41ccc578f37ebf0c4c43303
IvanRoperto <roperto@gmail.com>
2019-03-12 10:52:25 -0400

    Configuracion de envio de correo del formulario de contacto

--- ajax/contacto.php
+++ ajax/contacto.php
@@ -6,7 +6,34 @@ require '../phpmailer/src/Exception.php';
 require '../phpmailer/src/PHPMailer.php';
 require '../phpmailer/src/SMTP.php';
 
+function limpiarCadena($str)
+{
+  global $connection;
+  $str = mysqli_real_escape_string($connection, trim($str));
+  return htmlspecialchars($str);
+}
+
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+
+$email = isset($_POST['email'])? limpiarCadena($_POST['email']):"";
+$nombre = isset($_POST['nombre'])? limpiarCadena($_POST['nombre']):"";
+$asunto = isset($_POST['asunto'])? limpiarCadena($_POST['asunto']):"";
+$telefono = isset($_POST['telefono'])? limpiarCadena($_POST['telefono']):"";
+
+$mensaje = isset($_POST['mensaje'])? limpiarCadena($_POST['mensaje']):"";
+
+$mensaje = str_replace("\n\n", "</p>\n<p>", $mensaje);
+$mensaje = "<p>" . $mensaje . "</p>";
+
+$body_msg = "<div style='border: 4px solid navy; margin: 20px auto; padding: 30px;'>
+  <img src='http://hyjservices.com.do/img/logo.png' alt='logo' style='width: 200px; height: auto;'>
+  <h2 style='font-family: sans-serif;'>".$nombre."</h2>
+  <p style='font-family: sans-serif;'><b>Tel&eacute;fono:</b> ".$telefono."</p>
+  <p style='font-family: sans-serif;'><b>Mensaje:</b></p>
+  <div style='font-family: sans-serif; font-size: 14px;'>".$mensaje."
+  </div>
+</div>";
+
 try {
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
@@ -19,10 +46,10 @@ try {
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('roperto@gmail.com', 'Ivan Roperto');
+    $mail->setFrom($email, $nombre);
     $mail->addAddress('ventas@hyjservice.com', 'Formulario de Contacto');     // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('roperto@gmail.com', 'Ivan Roperto');
+    $mail->addReplyTo($email, $nombre);
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
 
@@ -33,8 +60,8 @@ try {
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Formulario de Contacto';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Body    = $body_msg;
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
