f0256cf6c512c422237b58a82b374c73877ae46d
IvanRoperto <roperto@gmail.com>
2019-03-12 09:53:19 -0400

    Correcion de errores en servicios

--- servicios.php
+++ servicios.php
@@ -6,22 +6,22 @@
   <h1>Servicios</h1>
   <div class="row products justify-content-center">
     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-      <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-        <img src="img/productos/servicio1.jpg" alt="Servicio 2" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
-        <h3>Entrega a Domicilio</h3>
-        <p>Ofrecemos entregas puerta a puerta a las principales ciudades del país en un plazo de 24 a 72 horas.</p>
-      </div>
-      <img src="img/productos/servicio2.jpg" alt="Servicio 1" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
+      <img src="img/productos/servicio1.jpg" alt="Entrega a Domicilio" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
+      <h3>Entrega a Domicilio</h3>
+      <p>Ofrecemos entregas puerta a puerta a las principales ciudades del país en un plazo de 24 a 72 horas.</p>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+      <img src="img/productos/servicio2.jpg" alt="Reparación de Mobiliario de Oficina" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
       <h3>Reparación de Mobiliario de Oficina</h3>
       <p>Reparación, Servicio y Mantenimiento de todo tipo de Muebles para Oficinas en general.</p>
     </div>
     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-      <img src="img/productos/servicio3.jpg" alt="Servicio 3" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
+      <img src="img/productos/servicio3.jpg" alt="Reparación de Impresoras" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
       <h3>Reparación de Impresoras</h3>
       <p>Reparamos todo tipo de Impresoras y fotocopiadoras, con un personal tecnico altamente calificado. </p>
     </div>
     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-      <img src="img/productos/servicio4.jpg" alt="Servicio 4" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
+      <img src="img/productos/servicio4.jpg" alt="Colocación de Impresoras" class="rounded-circle mx-auto d-block shadow p-2 img-fluid img-thumbnails">
       <h3>Colocación de Impresoras</h3>
       <p>Colocamos Impresoras en tu oficina sin costo de compra o mantenimiento, con el compromiso de comprar los suministros del impresor.</p>
     </div>
