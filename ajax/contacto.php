35e7b6638d7824bae89e33fbb2e0e5115011cb76
IvanRoperto <roperto@gmail.com>
2019-03-13 13:35:29 -0400

    cambio de correos a correo de la empresa formularios ventas y contacto

--- ajax/contacto.php
+++ ajax/contacto.php
@@ -39,8 +39,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'roperto@gmail.com';                 // SMTP username
-    $mail->Password = 'LiaMichelle@260313';                           // SMTP password
+    $mail->Username = 'ventas@hyjservice.com';                 // SMTP username
+    $mail->Password = 'admin123';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
